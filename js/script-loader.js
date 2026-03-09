1c74afe2cb26863c60364f68a1cf9b9305953773
beatriz-araujo <beatriz@forwwward.studio>
2026-03-09 13:09:00 +0000

    Removed DOMContentLoaded listener
    This was preventing script imports from running

--- js/script-loader.js
+++ js/script-loader.js
@@ -4,14 +4,12 @@ import { swiperInit } from './swiper.js';
 import { form } from './form.js';
 import { footerDate } from './footer-date.js';
 
-// not all script files are being loaded byt default (example: faqs...)
+// not all script files are being loaded by default (example: faqs...)
 
-document.addEventListener("DOMContentLoaded", function () {
-    mainInit();
-    navBarMenu();
-    swiperInit();
-    form();
-    footerDate();
-});
+mainInit();
+navBarMenu();
+swiperInit();
+form();
+footerDate();
 
 console.log("Loading main scripts loader");
